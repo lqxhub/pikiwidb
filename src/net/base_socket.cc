a8e0950d6c6e7f11565e239c3e77e44bb5cbcbc0
lqx <lqxlucky@qq.com>
2024-06-11 22:11:10 +0800

    Replacement network library

---
+++ src/net/base_socket.cc
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2023-present, Qihoo, Inc.  All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <fcntl.h>
+#include <netinet/tcp.h>
+#include <unistd.h>
+
+#include "base_socket.h"
+
+namespace net {
+
+int BaseSocket::CreateTCPSocket() { return ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); }
+
+int BaseSocket::CreateUDPSocket() { return ::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); }
+
+void BaseSocket::Close() {
+  auto fd = Fd();
+  if (fd_.compare_exchange_strong(fd, 0)) {
+    ::shutdown(fd, SHUT_RDWR);
+    ::close(fd);
+  }
+}
+
+void BaseSocket::OnCreate() {
+#ifndef HAVE_ACCEPT4
+  SetNonBlock(true);
+#endif
+  SetNodelay();
+  SetSndBuf();
+  SetRcvBuf();
+}
+
+void BaseSocket::SetNonBlock(bool noBlock) {
+  int flag = ::fcntl(Fd(), F_GETFL, 0);
+  if (-1 == flag) {
+    return;
+  }
+
+  if (noBlock) {
+    if (::fcntl(Fd(), F_SETFL, flag | O_NONBLOCK) != -1) {
+      noBlock_ = true;
+    }
+  } else {
+    if (::fcntl(Fd(), F_SETFL, flag & ~O_NONBLOCK) != -1) {
+      noBlock_ = false;
+    }
+  }
+}
+
+void BaseSocket::SetNodelay() {
+  int nodelay = 1;
+  ::setsockopt(Fd(), IPPROTO_TCP, TCP_NODELAY, reinterpret_cast<const char *>(&nodelay), sizeof(int));
+}
+
+void BaseSocket::SetSndBuf(socklen_t winsize) {
+  ::setsockopt(Fd(), SOL_SOCKET, SO_SNDBUF, reinterpret_cast<const char *>(&winsize), sizeof(winsize));
+}
+
+void BaseSocket::SetRcvBuf(socklen_t winsize) {
+  ::setsockopt(Fd(), SOL_SOCKET, SO_RCVBUF, reinterpret_cast<const char *>(&winsize), sizeof(winsize));
+}
+
+void BaseSocket::SetReuseAddr() {
+  int reuse = 1;
+  ::setsockopt(Fd(), SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char *>(&reuse), sizeof(reuse));
+}
+
+bool BaseSocket::SetReusePort() {
+  int reuse = 1;
+  return ::setsockopt(Fd(), SOL_SOCKET, SO_REUSEPORT, reinterpret_cast<const char *>(&reuse), sizeof(reuse)) != -1;
+}
+
+bool BaseSocket::GetLocalAddr(SocketAddr &addr) {
+  sockaddr_in localAddr{};
+  socklen_t len = sizeof(localAddr);
+
+  if (0 == ::getsockname(Fd(), reinterpret_cast<struct sockaddr *>(&localAddr), &len)) {
+    addr.Init(localAddr);
+  } else {
+    return false;
+  }
+
+  return true;
+}
+
+bool BaseSocket::GetPeerAddr(SocketAddr &addr) {
+  sockaddr_in remoteAddr{};
+  socklen_t len = sizeof(remoteAddr);
+  if (0 == ::getpeername(Fd(), reinterpret_cast<struct sockaddr *>(&remoteAddr), &len)) {
+    addr.Init(remoteAddr);
+  } else {
+    return false;
+  }
+
+  return true;
+}
+
+}  // namespace net
