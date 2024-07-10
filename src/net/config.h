bb644c59b6c32daf6a830e3530ffe05c61984ae0
lqx <lqxlucky@qq.com>
2024-07-10 23:03:20 +0800

    fix macos error

--- src/net/config.h
+++ src/net/config.h
@@ -13,12 +13,19 @@ namespace net {
 #  define HAVE_EPOLL 1
 #endif
 
-#if (defined(__APPLE__)) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
 #  define HAVE_KQUEUE 1
 #endif
 
 #ifdef __linux__
 #  define HAVE_ACCEPT4 1
+#endif
+
+#if defined(__x86_64__) || defined(_M_X64) || defined(__ppc64__) || defined(__aarch64__) || defined(__64BIT__) || \
+    defined(_LP64) || defined(__LP64__)
+#  define HAVE_64BIT 1
+#else
+#  define HAVE_32BIT 1
 #endif
 
 }  // namespace net
